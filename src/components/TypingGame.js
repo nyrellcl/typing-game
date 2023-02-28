fb0391ef028ea322016c2b02fcefbfa8018b5d55
nyrellcl <nyrell.leonor2000@gmail.com>
2023-02-27 20:58:12 -0800

    default quotes

--- src/components/TypingGame.js
+++ src/components/TypingGame.js
@@ -4,15 +4,13 @@ import { Link } from "react-router-dom";
 
 function TypingGame() {
   const [quotes] = useState([
-      "YAWA",
-      "BILAT"
-    /*"When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
+    "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
     "There is nothing more deceptive than an obvious fact.",
     "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
     "I never make exceptions. An exception disproves the rule.",
     "What one man can invent another can discover.",
     "Nothing clears up a case so much as stating it to another person.",
-    "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",*/
+    "Education never ends, Watson. It is a series of lessons, with the greatest for the last."
   ]);
 
   const [words, setWords] = useState([]);
