270098802b88abdd3f1c5e4fbba3774d235b8804
nyrellcl <nyrell.leonor2000@gmail.com>
2023-02-27 20:27:45 -0800

    add keyframes on keyboard css keys

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function StartPage() {
+  const [keyboards] = useState(['A', 'S', 'D', 'F'])
   return (
     <section className="start-section">
+      <div className="keys">
+        {keyboards.map((keyboard, keyboardIdx)=>(
+          <span className="keys__individual" key={keyboardIdx}>{keyboard}</span>
+        ))}
+      </div>
       <h1>Typing Game</h1>
       <article className="start-section__content">
         <p>Click start and test out your typing skills!</p>
